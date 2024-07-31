452291d5a19457c39ebe1ed4fb451fc4b6930930
solidol <magnet87@gmail.com>
2024-07-31 13:30:06 +0300

    ryre

--- resources/views/teachers/show.blade.php
+++ resources/views/teachers/show.blade.php
@@ -12,7 +12,40 @@
             </div>
             <div class="col-12 col-md-8 p-2">
                 <h2 class="text-center name-3">{{ $teacher->fullname }}</h2>
+                <div class="form-group row mb-2">
+                    <label for="email" class="col-3 col-form-label text-md-right">Контактний e-mail</label>
+                    <div class="col-6">
+                        <input type="email" class="form-control" name="email" id="email"
+                            value="{{ $teacher->user->email }}" readonly>
+                    </div>
+                </div>
+                <div class="form-group row mb-2">
+                    <label for="skype" class="col-3 col-form-label text-md-right">Skype</label>
+                    <div class="col-6">
+                        <input type="text" class="form-control" name="skype" id="skype"
+                            value="{{ $teacher->user->skype_id }}" readonly>
+                    </div>
+                </div>
+                <div class="form-group row mb-2">
+                    <label for="viber" class="col-3 col-form-label text-md-right">Viber</label>
+                    <div class="col-6">
+                        <input type="text" class="form-control" name="viber" id="viber"
+                            value="{{ $teacher->user->viber_id }}" readonly>
+                    </div>
+                    <div class="col-3 col-form-label text-danger">
 
+                    </div>
+                </div>
+                <div class="form-group row mb-2">
+                    <label for="skype" class="col-3 col-form-label text-md-right">Telegram</label>
+                    <div class="col-6">
+                        <input type="telegram" class="form-control" name="telegram" id="telegram"
+                            value="{{ $teacher->user->telegram_id }}" readonly>
+                    </div>
+                    <div class="col-3 col-form-label text-danger">
+
+                    </div>
+                </div>
                 @foreach ($teacher->journalsByGroup($group->kod_grup) as $journal)
                     <div class="m-2">
                         <a class="btn btn-primary d-block" href="{{ route('journals.show', ['journal' => $journal]) }}">
