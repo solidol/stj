9e3e1a57e8cc87a65c5c5f42b63935b580a4d48f
solidol <magnet87@gmail.com>
2024-08-10 19:05:44 +0300

    sfsdfs

--- resources/views/menus/mainmenu.blade.php
+++ resources/views/menus/mainmenu.blade.php
@@ -2,7 +2,7 @@
     @if (Auth::user()->isStudent())
         @include('menus.main_student')
     @elseif (Auth::user()->isAdmin())
-        @include('menus.main_guest')
+        @include('menus.main_admin')
     @else
         @include('menus.main_guest')
     @endif
