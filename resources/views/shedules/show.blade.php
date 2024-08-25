7f3bdea6064b2920a4de3c94fabfbf02fcc05c17
solidol <magnet87@gmail.com>
2024-08-25 15:07:27 +0300

    fgdfg

--- resources/views/shedules/show.blade.php
+++ resources/views/shedules/show.blade.php
@@ -15,7 +15,7 @@
             ?>
             <div class="col-xl-3 col-md-6 col-sm-12 {{ $dw == $d ? 'bg-light ' : '' }}">
 
-                <div class="m-2">
+                <div class="p-2">
                     <h2 class="fs-2 text-white">
                         {{ App\Library\CalendarHelper::$daysofweek[$d] }}
                     </h2>
