78a9f915bdb68eca048a74fba59aff474f378b15
solidol <magnet87@gmail.com>
2024-08-25 15:47:41 +0300

    rtretwre

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('title', 'Помилка 404')
 @section('content')
-<div class="container">
+<div class="container">111
     <div class="row justify-content-center">
         <div class="col-md-6">
             <div class="card">
