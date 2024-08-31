c02b4f8cdabba6858ba6a1e148c234b0aebd7f89
solidol <magnet87@gmail.com>
2024-08-31 20:53:35 +0300

    dfdsfds

--- resources/views/shedules/show.blade.php
+++ resources/views/shedules/show.blade.php
@@ -6,14 +6,14 @@
 
     <div class="row">
 
-        @for ($d = 1; $d <= 7; $d++)
+        @for ($d = 1; $d <= 6; $d++)
             <?php
             $dw = date('w');
             if ($dw == 0) {
                 $dw = 7;
             }
             ?>
-            <div class="col-xl-3 col-md-6 col-sm-12 my-2">
+            <div class="col-xl-4 col-md-6 col-sm-12 my-2">
 
                 <div class="rounded-1 bg-light {{ $dw == $d ? 'border border-3 border-danger' : '' }}" style="height:100%">
                     <h2 class="fs-2">
